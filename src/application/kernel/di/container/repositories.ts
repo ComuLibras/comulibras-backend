72660ac8448cad988744aae7b2ca30a378795bfd
Amauri <amauri.plimaj@gmail.com>
2025-06-02 19:18:39 -0300

    feat: register PrismaCategoryRepository and PrismaSentenceRepository in the DI container

--- src/application/kernel/di/container/repositories.ts
+++ src/application/kernel/di/container/repositories.ts
@@ -1,6 +1,10 @@
 import { container } from '.';
 import { PrismaAccountRepository } from '../../../domain/accounts/repositories/prisma-account-respository';
+import { PrismaCategoryRepository } from '../../../domain/categories/repositories/prisma-category-repository';
+import { PrismaSentenceRepository } from '../../../domain/sentences/repositories/prisma-sentence-respository';
 
 export function registerRepositories() {
   container.register(PrismaAccountRepository, 'AccountRepository');
+  container.register(PrismaCategoryRepository, 'CategoryRepository');
+  container.register(PrismaSentenceRepository, 'SentenceRepository');
 }

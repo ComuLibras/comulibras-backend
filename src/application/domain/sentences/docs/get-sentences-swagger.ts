fbbf56c2aa0f8ed46b1b161d3625db6a6b1025be
Amauri <amauri.plimaj@gmail.com>
2025-06-02 22:12:22 -0300

    feat: add Swagger documentation for get sentences endpoint

---
+++ src/application/domain/sentences/docs/get-sentences-swagger.ts
@@ -0,0 +1,54 @@
+import { Operation } from 'swagger-jsdoc';
+import { Tags } from '../../../shared/docs/tags';
+
+export const getSentencesSwagger: Operation = {
+  tags: [Tags.SENTENCES],
+  summary: 'Listar frases',
+  description: 'Lista todas as frases',
+  security: [],
+  parameters: [
+    {
+      name: 'page',
+      in: 'query',
+      required: false,
+      schema: {
+        type: 'number',
+      },
+    },
+    {
+      name: 'limit',
+      in: 'query',
+      required: false,
+      schema: {
+        type: 'number',
+      },
+    },
+    {
+      name: 'categoryId',
+      in: 'query',
+      required: false,
+      schema: {
+        type: 'string',
+        format: 'uuid',
+      },
+    },
+    {
+      name: 'search',
+      in: 'query',
+      required: false,
+      schema: {
+        type: 'string',
+      },
+    },
+  ],
+  responses: {
+    '200': {
+      description: 'Frases listadas com sucesso',
+      content: {
+        'application/json': {
+          schema: { $ref: '#/components/schemas/Sentence' },
+        },
+      },
+    },
+  },
+};
--- src/application/domain/sentences/docs/sentences-path.ts
+++ src/application/domain/sentences/docs/sentences-path.ts
@@ -1,6 +1,9 @@
 import { PathItem } from 'swagger-jsdoc';
+import { addDefaultResponsesSwagger } from '../../../shared/docs/add-default-responses-swagger';
+import { getSentencesSwagger } from './get-sentences-swagger';
 
 export const sentencesPath: PathItem = {
+  get: addDefaultResponsesSwagger(getSentencesSwagger, { omitResponses: ['401', '403'] }),
 };
 
 export const sentencesPathWithId: PathItem = {

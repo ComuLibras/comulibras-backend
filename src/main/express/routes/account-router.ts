9cc5ab4994697134521eaff390234503f2bba9fa
Amauri <amauri.plimaj@gmail.com>
2025-07-06 17:52:51 -0300

    feat: add PATCH route for updating account status in accountsRouter

--- src/main/express/routes/account-router.ts
+++ src/main/express/routes/account-router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateAccountController } from '@domain/accounts/use-cases/create-account/create-account-controller';
 import { UpdateAccountRoleController } from '@domain/accounts/use-cases/update-account-role/update-account-role-controller';
+import { UpdateAccountStatusController } from '@domain/accounts/use-cases/update-account-status/update-account-status-controller';
 
 import { container } from '@kernel/di/container';
 
@@ -11,3 +12,4 @@ export const accountsRouter = Router();
 
 accountsRouter.post('/',  routeAdapter(container.resolve(CreateAccountController)));
 accountsRouter.patch('/:accountId/role',  routeAdapter(container.resolve(UpdateAccountRoleController)));
+accountsRouter.patch('/:accountId/status',  routeAdapter(container.resolve(UpdateAccountStatusController)));

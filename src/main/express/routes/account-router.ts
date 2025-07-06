b7c0136610f342bb2ceaaf511f729d22b0c6e1a3
Amauri <amauri.plimaj@gmail.com>
2025-07-06 18:08:48 -0300

    feat: add DELETE route for account deletion in accountsRouter

--- src/main/express/routes/account-router.ts
+++ src/main/express/routes/account-router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateAccountController } from '@domain/accounts/use-cases/create-account/create-account-controller';
+import { DeleteAccountController } from '@domain/accounts/use-cases/delete-account/delete-account-controller';
 import { UpdateAccountRoleController } from '@domain/accounts/use-cases/update-account-role/update-account-role-controller';
 import { UpdateAccountStatusController } from '@domain/accounts/use-cases/update-account-status/update-account-status-controller';
 
@@ -13,3 +14,4 @@ export const accountsRouter = Router();
 accountsRouter.post('/',  routeAdapter(container.resolve(CreateAccountController)));
 accountsRouter.patch('/:accountId/role',  routeAdapter(container.resolve(UpdateAccountRoleController)));
 accountsRouter.patch('/:accountId/status',  routeAdapter(container.resolve(UpdateAccountStatusController)));
+accountsRouter.delete('/:accountId',  routeAdapter(container.resolve(DeleteAccountController)));

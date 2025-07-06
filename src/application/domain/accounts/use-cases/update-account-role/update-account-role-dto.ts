e29188146d229598659cfd403427b4668bd5ff5a
Amauri <amauri.plimaj@gmail.com>
2025-07-06 17:48:28 -0300

    feat: add validation schema for updating account role parameters

--- src/application/domain/accounts/use-cases/update-account-role/update-account-role-dto.ts
+++ src/application/domain/accounts/use-cases/update-account-role/update-account-role-dto.ts
@@ -3,10 +3,15 @@ import { z } from 'zod';
 
 import { Roles } from '@domain/accounts/entities/role';
 
+export const updateAccountRoleParams = z.object({
+  accountId: z.string().uuid(),
+});
+
+export type UpdateAccountRoleParams = z.infer<typeof updateAccountRoleParams>;
+
 export const updateAccountRoleBody = z.object({
   role: z.nativeEnum(Roles),
 });
 
 export type UpdateAccountRoleBody = z.infer<typeof updateAccountRoleBody>;
-
 export const updateAccountRoleOpenAPIBody = generateSchema(updateAccountRoleBody);

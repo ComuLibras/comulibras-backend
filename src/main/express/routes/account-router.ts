c9b833c683bc7543c0ba75990bc970be40d38743
Amauri <amauri.plimaj@gmail.com>
2025-07-06 18:24:13 -0300

    feat: add GET route for retrieving accounts in accountsRouter

--- src/main/express/routes/account-router.ts
+++ src/main/express/routes/account-router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateAccountController } from '@domain/accounts/use-cases/create-account/create-account-controller';
 import { DeleteAccountController } from '@domain/accounts/use-cases/delete-account/delete-account-controller';
+import { GetAccountsController } from '@domain/accounts/use-cases/get-accounts/get-accounts-controller';
 import { UpdateAccountRoleController } from '@domain/accounts/use-cases/update-account-role/update-account-role-controller';
 import { UpdateAccountStatusController } from '@domain/accounts/use-cases/update-account-status/update-account-status-controller';
 
@@ -12,6 +13,7 @@ import { routeAdapter } from '../adapters/route-adapter';
 export const accountsRouter = Router();
 
 accountsRouter.post('/',  routeAdapter(container.resolve(CreateAccountController)));
+accountsRouter.get('/',  routeAdapter(container.resolve(GetAccountsController)));
 accountsRouter.patch('/:accountId/role',  routeAdapter(container.resolve(UpdateAccountRoleController)));
 accountsRouter.patch('/:accountId/status',  routeAdapter(container.resolve(UpdateAccountStatusController)));
 accountsRouter.delete('/:accountId',  routeAdapter(container.resolve(DeleteAccountController)));

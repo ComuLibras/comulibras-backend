090361642fd05cc95dba5294cefd3617c0a26289
Amauri <amauri.plimaj@gmail.com>
2025-07-06 17:52:14 -0300

    feat: add validation schema for updating account status parameters

--- src/application/domain/accounts/use-cases/update-account-status/update-account-status-dto.ts
+++ src/application/domain/accounts/use-cases/update-account-status/update-account-status-dto.ts
@@ -1,10 +1,15 @@
 import { generateSchema } from '@anatine/zod-openapi';
 import { z } from 'zod';
 
+export const updateAccountStatusParams = z.object({
+  accountId: z.string().uuid(),
+});
+
+export type UpdateAccountStatusParams = z.infer<typeof updateAccountStatusParams>;
+
 export const updateAccountStatusBody = z.object({
   isActive: z.boolean(),
 });
 
 export type UpdateAccountStatusBody = z.infer<typeof updateAccountStatusBody>;
-
 export const updateAccountStatusOpenAPIBody = generateSchema(updateAccountStatusBody);

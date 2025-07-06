dc478bda08fd662b82623ad70b78a6dfee575efd
Amauri <amauri.plimaj@gmail.com>
2025-07-06 20:32:45 -0300

    feat: add DELETE route to sentences router for sentence deletion requests

--- src/main/express/routes/sentences-router.ts
+++ src/main/express/routes/sentences-router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateSentenceController } from '@domain/sentences/use-cases/create-sentence/create-sentence-controller';
+import { DeleteSentenceController } from '@domain/sentences/use-cases/delete-sentence/delete-sentence-controller';
 
 import { container } from '@kernel/di/container';
 
@@ -9,3 +10,4 @@ import { routeAdapter } from '../adapters/route-adapter';
 export const sentencesRouter = Router();
 
 sentencesRouter.post('/', routeAdapter(container.resolve(CreateSentenceController)));
+sentencesRouter.delete('/:sentenceId', routeAdapter(container.resolve(DeleteSentenceController)));

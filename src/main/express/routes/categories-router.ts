210d305a6cebedf8fcbd01d438c6692e4a04301a
Amauri <amauri.plimaj@gmail.com>
2025-07-06 18:48:39 -0300

    feat: add PUT route for updating categories using UpdateCategoryController

--- src/main/express/routes/categories-router.ts
+++ src/main/express/routes/categories-router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateCategoryController } from '@domain/categories/use-cases/create-category/create-category-controller';
+import { UpdateCategoryController } from '@domain/categories/use-cases/update-category/update-category-controller';
 
 import { container } from '@kernel/di/container';
 
@@ -9,3 +10,4 @@ import { routeAdapter } from '../adapters/route-adapter';
 export const categoriesRouter = Router();
 
 categoriesRouter.post('/',  routeAdapter(container.resolve(CreateCategoryController)));
+categoriesRouter.put('/:categoryId',  routeAdapter(container.resolve(UpdateCategoryController)));

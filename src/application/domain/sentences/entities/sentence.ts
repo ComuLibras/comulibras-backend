a87416070b4e80c86591532113a24b5bb08dd97a
Amauri <amauri.plimaj@gmail.com>
2025-07-06 20:22:40 -0300

    feat: enhance Sentence entity with Prisma integration and HTTP schema mapping

--- src/application/domain/sentences/entities/sentence.ts
+++ src/application/domain/sentences/entities/sentence.ts
@@ -1,9 +1,13 @@
+import { Prisma } from '@prisma/client';
+
 import { Entity, IEntityProps } from '@shared/entities/entity';
+
+import { SentenceHttpSchema } from '../mappers/sentence-mapper';
 
 interface ISentenceProps extends IEntityProps {
   content: string;
   videoUrl: string;
-  categoryId: string;
+  categoryId?: string | null;
   isActive: boolean;
 }
 
@@ -15,19 +19,35 @@ export class Sentence extends Entity {
     this.props = props;
   }
 
-  public get content(): string {
-    return this.props.content;
+  toPrisma(): Prisma.SentenceCreateInput {
+    const domain = this.props;
+
+    return {
+      id: domain.id,
+      content: domain.content,
+      video_url: domain.videoUrl,
+      category: domain.categoryId ? {
+        connect: {
+          id: domain.categoryId,
+        },
+      } : undefined,
+      isActive: domain.isActive,
+      createdAt: domain.createdAt,
+      updatedAt: domain.updatedAt,
+    };
   }
 
-  public get videoUrl(): string {
-    return this.props.videoUrl;
-  }
+  toHttp(): SentenceHttpSchema {
+    const domain = this.props;
 
-  public get categoryId(): string {
-    return this.props.categoryId;
-  }
-
-  public get isActive(): boolean {
-    return this.props.isActive;
+    return {
+      id: domain.id ?? '',
+      content: domain.content,
+      videoUrl: domain.videoUrl,
+      categoryId: domain.categoryId ?? null,
+      isActive: domain.isActive,
+      createdAt: domain.createdAt ?? new Date(),
+      updatedAt: domain.updatedAt ?? new Date(),
+    };
   }
 }

fa19d8800da7293b6a9faa2a53bf127bf50344de
Amauri <amauri.plimaj@gmail.com>
2025-07-06 19:24:15 -0300

    feat: add DTO for updating category status with validation schema

--- src/application/domain/categories/use-cases/update-category-status/update-category-status-dto.ts
+++ src/application/domain/categories/use-cases/update-category-status/update-category-status-dto.ts
@@ -1,6 +1,12 @@
 import { generateSchema } from '@anatine/zod-openapi';
 import { z } from 'zod';
 
+export const updateCategoryStatusParams = z.object({
+  categoryId: z.string().uuid(),
+});
+
+export type UpdateCategoryStatusParams = z.infer<typeof updateCategoryStatusParams>;
+
 export const updateCategoryStatusBody = z.object({
   isActive: z.boolean(),
 });

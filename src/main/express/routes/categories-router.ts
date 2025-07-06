fb4947f49a1a124f2d84a5bea7178441aba83d77
Amauri <amauri.plimaj@gmail.com>
2025-07-06 19:26:16 -0300

    feat: add PATCH route for updating category status using

--- src/main/express/routes/categories-router.ts
+++ src/main/express/routes/categories-router.ts
@@ -4,6 +4,7 @@ import { CreateCategoryController } from '@domain/categories/use-cases/create-ca
 import { DeleteCategoryController } from '@domain/categories/use-cases/delete-category/delete-category-controller';
 import { GetCategoriesController } from '@domain/categories/use-cases/get-categories/get-categories-controller';
 import { UpdateCategoryController } from '@domain/categories/use-cases/update-category/update-category-controller';
+import { UpdateCategoryStatusController } from '@domain/categories/use-cases/update-category-status/update-category-status-controller';
 
 import { container } from '@kernel/di/container';
 
@@ -15,3 +16,4 @@ categoriesRouter.get('/', routeAdapter(container.resolve(GetCategoriesController
 categoriesRouter.post('/', routeAdapter(container.resolve(CreateCategoryController)));
 categoriesRouter.put('/:categoryId', routeAdapter(container.resolve(UpdateCategoryController)));
 categoriesRouter.delete('/:categoryId', routeAdapter(container.resolve(DeleteCategoryController)));
+categoriesRouter.patch('/:categoryId', routeAdapter(container.resolve(UpdateCategoryStatusController)));

78c95ff16496649baaaa8709bdd68295678d5a52
Amauri <amauri.plimaj@gmail.com>
2025-07-06 22:28:31 -0300

    feat: add validation schema for updating sentence status parameters

--- src/application/domain/sentences/use-cases/update-sentence-status/update-sentence-status-dto.ts
+++ src/application/domain/sentences/use-cases/update-sentence-status/update-sentence-status-dto.ts
@@ -1,6 +1,12 @@
 import { generateSchema } from '@anatine/zod-openapi';
 import { z } from 'zod';
 
+export const updateSentenceStatusParams = z.object({
+  sentenceId: z.string().uuid(),
+});
+
+export type UpdateSentenceStatusParams = z.infer<typeof updateSentenceStatusParams>;
+
 export const updateSentenceStatusBody = z.object({
   isActive: z.boolean(),
 });

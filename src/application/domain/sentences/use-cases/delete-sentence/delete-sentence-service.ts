c1b09bf2f58fcbf8e80edae9576809036d1cb296
Amauri <amauri.plimaj@gmail.com>
2025-07-06 22:23:26 -0300

    refactor: simplify DeleteSentenceService by removing category-related logic and imports

--- src/application/domain/sentences/use-cases/delete-sentence/delete-sentence-service.ts
+++ src/application/domain/sentences/use-cases/delete-sentence/delete-sentence-service.ts
@@ -1,8 +1,5 @@
 
-import { CATEGORY_NOT_FOUND_ERROR } from '@domain/categories/docs/contants';
-import { ICategoryRepository } from '@domain/categories/repositories/category-repository';
 import { SENTENCE_NOT_FOUND_ERROR } from '@domain/sentences/docs/constants';
-import { Sentence } from '@domain/sentences/entities/sentence';
 import { ISentenceRepository } from '@domain/sentences/repositories/sentence-repository';
 
 import { Inject } from '@kernel/decorators/inject';
@@ -17,34 +14,19 @@ import { DeleteSentenceParams } from './delete-sentence-dto';
 export class DeleteSentenceService implements IService<DeleteSentenceService.Input, DeleteSentenceService.Output> {
   constructor(
     @Inject('SentenceRepository') private readonly sentenceRepository: ISentenceRepository,
-    @Inject('CategoryRepository') private readonly categoryRepository: ICategoryRepository,
   ) {}
 
   async execute(input: DeleteSentenceService.Input): Promise<DeleteSentenceService.Output> {
-    const sentence = await this.validateSentence(input.sentenceId);
+    await this.validateSentence(input.sentenceId);
 
     await this.sentenceRepository.delete(input.sentenceId);
-
-    if (sentence.props.categoryId) {
-      await this.decreaseCategorySentenceCount(sentence.props.categoryId);
-    }
   }
 
-  private async validateSentence(sentenceId: string): Promise<Sentence> {
+  private async validateSentence(sentenceId: string): Promise<void> {
     const sentence = await this.sentenceRepository.findById(sentenceId);
     if (!sentence) {
       throw new NotFoundHTTPError(SENTENCE_NOT_FOUND_ERROR);
     }
-    return sentence;
-  }
-
-  private async decreaseCategorySentenceCount(categoryId: string): Promise<void> {
-    const category = await this.categoryRepository.findById(categoryId);
-    if (!category) {
-      throw new NotFoundHTTPError(CATEGORY_NOT_FOUND_ERROR);
-    }
-    category.decreaseSentenceCount();
-    await this.categoryRepository.update(category);
   }
 }
 

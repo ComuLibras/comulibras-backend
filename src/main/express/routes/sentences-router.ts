e82457d2df4b77e0c9dc33bb58a56ea6e5cdbf94
Amauri <amauri.plimaj@gmail.com>
2025-07-06 22:40:33 -0300

    feat: add PUT route for updating sentences in sentences router

--- src/main/express/routes/sentences-router.ts
+++ src/main/express/routes/sentences-router.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateSentenceController } from '@domain/sentences/use-cases/create-sentence/create-sentence-controller';
 import { DeleteSentenceController } from '@domain/sentences/use-cases/delete-sentence/delete-sentence-controller';
 import { DeleteSentencesController } from '@domain/sentences/use-cases/delete-sentences/delete-sentences-controller';
+import { UpdateSentenceController } from '@domain/sentences/use-cases/update-sentence/update-sentence-controller';
 import { UpdateSentenceStatusController } from '@domain/sentences/use-cases/update-sentence-status/update-sentence-status-controller';
 
 import { container } from '@kernel/di/container';
@@ -12,6 +13,7 @@ import { routeAdapter } from '../adapters/route-adapter';
 export const sentencesRouter = Router();
 
 sentencesRouter.post('/', routeAdapter(container.resolve(CreateSentenceController)));
+sentencesRouter.put('/:sentenceId', routeAdapter(container.resolve(UpdateSentenceController)));
 sentencesRouter.patch('/:sentenceId', routeAdapter(container.resolve(UpdateSentenceStatusController)));
 sentencesRouter.delete('/', routeAdapter(container.resolve(DeleteSentencesController)));
 sentencesRouter.delete('/:sentenceId', routeAdapter(container.resolve(DeleteSentenceController)));

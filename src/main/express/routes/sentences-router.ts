023a21cd392ea27214e04878936622e6edd9af16
Amauri <amauri.plimaj@gmail.com>
2025-07-06 22:48:54 -0300

    feat: add PATCH route for updating sentence categories in sentences router

--- src/main/express/routes/sentences-router.ts
+++ src/main/express/routes/sentences-router.ts
@@ -5,6 +5,7 @@ import { DeleteSentenceController } from '@domain/sentences/use-cases/delete-sen
 import { DeleteSentencesController } from '@domain/sentences/use-cases/delete-sentences/delete-sentences-controller';
 import { UpdateSentenceController } from '@domain/sentences/use-cases/update-sentence/update-sentence-controller';
 import { UpdateSentenceStatusController } from '@domain/sentences/use-cases/update-sentence-status/update-sentence-status-controller';
+import { UpdateSentencesCategoryController } from '@domain/sentences/use-cases/update-sentences-category/update-sentences-category-controller';
 
 import { container } from '@kernel/di/container';
 
@@ -14,6 +15,7 @@ export const sentencesRouter = Router();
 
 sentencesRouter.post('/', routeAdapter(container.resolve(CreateSentenceController)));
 sentencesRouter.put('/:sentenceId', routeAdapter(container.resolve(UpdateSentenceController)));
+sentencesRouter.patch('/', routeAdapter(container.resolve(UpdateSentencesCategoryController)));
 sentencesRouter.patch('/:sentenceId', routeAdapter(container.resolve(UpdateSentenceStatusController)));
 sentencesRouter.delete('/', routeAdapter(container.resolve(DeleteSentencesController)));
 sentencesRouter.delete('/:sentenceId', routeAdapter(container.resolve(DeleteSentenceController)));

5ac6558540ba2d1faf11f9d0eddbf703d2d8426b
Amauri <amauri.plimaj@gmail.com>
2025-07-06 22:40:07 -0300

    feat: add validation schema for updating sentence parameters with sentenceId

--- src/application/domain/sentences/use-cases/update-sentence/update-sentence-dto.ts
+++ src/application/domain/sentences/use-cases/update-sentence/update-sentence-dto.ts
@@ -3,6 +3,12 @@ import { z } from 'zod';
 
 import { createSentenceBody } from '../create-sentence/create-sentence-dto';
 
+export const updateSentenceParams = z.object({
+  sentenceId: z.string().uuid(),
+});
+
+export type UpdateSentenceParams = z.infer<typeof updateSentenceParams>;
+
 export const updateSentenceBody = createSentenceBody.partial();
 
 export type UpdateSentenceBody = z.infer<typeof updateSentenceBody>;

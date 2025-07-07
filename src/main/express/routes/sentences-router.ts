9f925ce56b4f1c6a30e15b1ac1ba730385f0d062
Amauri <amauri.plimaj@gmail.com>
2025-07-06 22:24:06 -0300

    feat: add DELETE route for bulk sentence deletion in sentences router

--- src/main/express/routes/sentences-router.ts
+++ src/main/express/routes/sentences-router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateSentenceController } from '@domain/sentences/use-cases/create-sentence/create-sentence-controller';
 import { DeleteSentenceController } from '@domain/sentences/use-cases/delete-sentence/delete-sentence-controller';
+import { DeleteSentencesController } from '@domain/sentences/use-cases/delete-sentences/delete-sentences-controller';
 
 import { container } from '@kernel/di/container';
 
@@ -10,4 +11,5 @@ import { routeAdapter } from '../adapters/route-adapter';
 export const sentencesRouter = Router();
 
 sentencesRouter.post('/', routeAdapter(container.resolve(CreateSentenceController)));
+sentencesRouter.delete('/', routeAdapter(container.resolve(DeleteSentencesController)));
 sentencesRouter.delete('/:sentenceId', routeAdapter(container.resolve(DeleteSentenceController)));

2d50349419725a9dc98562c462012b3ede736c53
Amauri <amauri.plimaj@gmail.com>
2025-07-06 23:04:55 -0300

    feat: add GET route to sentences router for retrieving sentences

--- src/main/express/routes/sentences-router.ts
+++ src/main/express/routes/sentences-router.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { CreateSentenceController } from '@domain/sentences/use-cases/create-sentence/create-sentence-controller';
 import { DeleteSentenceController } from '@domain/sentences/use-cases/delete-sentence/delete-sentence-controller';
 import { DeleteSentencesController } from '@domain/sentences/use-cases/delete-sentences/delete-sentences-controller';
+import { GetSentencesController } from '@domain/sentences/use-cases/get-sentences/get-sentences-controller';
 import { UpdateSentenceController } from '@domain/sentences/use-cases/update-sentence/update-sentence-controller';
 import { UpdateSentenceStatusController } from '@domain/sentences/use-cases/update-sentence-status/update-sentence-status-controller';
 import { UpdateSentencesCategoryController } from '@domain/sentences/use-cases/update-sentences-category/update-sentences-category-controller';
@@ -13,6 +14,7 @@ import { routeAdapter } from '../adapters/route-adapter';
 
 export const sentencesRouter = Router();
 
+sentencesRouter.get('/', routeAdapter(container.resolve(GetSentencesController)));
 sentencesRouter.post('/', routeAdapter(container.resolve(CreateSentenceController)));
 sentencesRouter.put('/:sentenceId', routeAdapter(container.resolve(UpdateSentenceController)));
 sentencesRouter.patch('/', routeAdapter(container.resolve(UpdateSentencesCategoryController)));

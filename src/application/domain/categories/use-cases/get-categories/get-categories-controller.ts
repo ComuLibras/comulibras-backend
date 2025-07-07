1f94212aa1eb17ce6a26499f5955973175b77e1b
Amauri <amauri.plimaj@gmail.com>
2025-07-07 11:14:49 -0300

    feat: enhance GetCategoriesController to handle query parameters and integrate query

--- src/application/domain/categories/use-cases/get-categories/get-categories-controller.ts
+++ src/application/domain/categories/use-cases/get-categories/get-categories-controller.ts
@@ -4,14 +4,17 @@ import { CategoryHttpSchema, CategoryMapper } from '@domain/categories/mappers/c
 import { HttpResponse } from '@kernel/decorators/http-response';
 import { Inject } from '@kernel/decorators/inject';
 import { Injectable } from '@kernel/decorators/injectable';
+import { Schema } from '@kernel/decorators/schema';
 
 import { Controller } from '@shared/http/interfaces/controller';
 import { StatusCode } from '@shared/http/status-code';
 
+import { GetCategoriesQuery, getCategoriesQuery } from './get-categories-dto';
 import { GetCategoriesService } from './get-categories-service';
 
 @Injectable()
 @HttpResponse(StatusCode.OK)
+@Schema({ query: getCategoriesQuery })
 export class GetCategoriesController extends Controller<CategoryHttpSchema[]> {
   constructor(
     @Inject('GetCategoriesService')
@@ -20,8 +23,11 @@ export class GetCategoriesController extends Controller<CategoryHttpSchema[]> {
     super();
   }
 
-  protected override async handle(): Controller.HandleResponse<CategoryHttpSchema[]> {
-    const categories = await this.getCategoriesService.execute();
+  protected override async handle(request: Http.Request<never, GetCategoriesQuery>): Controller.HandleResponse<CategoryHttpSchema[]> {
+    const categories = await this.getCategoriesService.execute({
+      account: request.account,
+      ...request.query,
+    });
 
     return categories.map(CategoryMapper.toHttp);
   }

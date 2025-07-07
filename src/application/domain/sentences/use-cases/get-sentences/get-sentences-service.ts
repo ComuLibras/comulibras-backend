9642b18c2c0211fba5d41eb211c6c3d96c6ee1ed
Amauri <amauri.plimaj@gmail.com>
2025-07-06 23:04:40 -0300

    feat: implement GetSentencesService to handle retrieval of sentences with total count

---
+++ src/application/domain/sentences/use-cases/get-sentences/get-sentences-service.ts
@@ -0,0 +1,30 @@
+import { IGetSentencesResponse, ISentenceRepository } from '@domain/sentences/repositories/sentence-repository';
+
+import { Inject } from '@kernel/decorators/inject';
+import { Injectable } from '@kernel/decorators/injectable';
+
+import { IService } from '@shared/http/interfaces/service';
+
+import { GetSentencesQuery } from './get-sentences-dto';
+
+@Injectable()
+export class GetSentencesService implements IService<GetSentencesService.Input, GetSentencesService.Output> {
+  constructor(
+    @Inject('SentenceRepository')
+    private readonly sentenceRepo: ISentenceRepository,
+  ) {}
+
+  async execute(input: GetSentencesService.Input): Promise<GetSentencesService.Output> {
+    const { sentences, totalSentences } = await this.sentenceRepo.findAll(input);
+
+    return {
+      sentences: sentences,
+      totalSentences,
+    };
+  }
+}
+
+export namespace GetSentencesService {
+  export type Input = GetSentencesQuery;
+  export type Output = IGetSentencesResponse;
+}
